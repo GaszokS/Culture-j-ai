cfc04abf5deeff87b15b829c5aa7d078eb1779a1
GazsokS <quentin.raemdonck@gmail.com>
2022-05-25 00:37:05 +0200

    Debut db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Import
 const express = require("express");
 const consolidate = require('consolidate');
+const bodyParser = require('body-parser')
+const crypto = require('crypto');
+const secret = 'SECRETKEY';
+
+const db = require('./db');
 
 // App Config
 const app = express();
-app.use(express.static("public"));
+app.use(
+    bodyParser.urlencoded({ extended: true }),
+    express.static('content')
+    );
 app.set('views','Content', 'ejs');
 app.engine('html', consolidate.hogan)
 app.listen(3000);
@@ -12,4 +20,27 @@ app.listen(3000);
 // Routes
 app.get("/", function(req, res){
     res.render("index.html");
+});
+
+app.get("/login", function(req, res){
+    res.render("login.html");    
+});
+
+app.post("/login", function(req, res){
+    const sha256Hasher = crypto.createHmac("sha256", secret);
+    const hash = sha256Hasher.update(req.body.password).digest("hex");
+    console.log(hash);
+    res.render("login.html");
+});
+
+app.get("/register", function(req, res){
+    res.render("register.html");
+});
+
+app.post("/register", function(req, res){
+    const sha256Hasher = crypto.createHmac("sha256", secret);
+    const hash = sha256Hasher.update(req.body.password).digest("hex");
+    db.addToDB("mydb", "users", {username: req.body.id, password: hash});
+    console.log(req.body);
+    res.render("register.html");
 });
