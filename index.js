8216ee2a9b6ba098deb782cfc5be7aa4280ef596
juliengourgue <gourguejulien@gmail.com>
2022-05-25 00:39:03 +0200

    Search bar

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // Import
 const express = require("express");
 const consolidate = require('consolidate');
-
+const bodyParser = require('body-parser')
 // App Config
 const app = express();
-app.use(express.static("public"));
-app.set('views','Content', 'ejs');
 app.engine('html', consolidate.hogan)
-app.listen(3000);
+app.set('views','Content');
+app.use(
+    bodyParser.urlencoded({ extended: true }),
+    express.static('content')
+);
+
+
 
 // Routes
 app.get("/", function(req, res){
     res.render("index.html");
 });
+
+app.post("/", function(req, res){
+    var inputs = req.body.search;
+    console.log(inputs);
+    res.render("index.html");
+});
+
+
+app.listen(3000);
