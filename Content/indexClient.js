a7054e7fe378a7a52dfe61d713e4d9776521277e
juliengourgue <gourguejulien@gmail.com>
2022-06-02 23:19:26 +0200

    add button to login logout

--- Content/indexClient.js
+++ Content/indexClient.js
@@ -1,3 +1,13 @@
 function emptySearchBar() {
     document.getElementById("searchForm").reset();
  }
+
+ function register(user){
+     if (user) {
+            document.getElementById("Login").style.display = "none";
+            document.getElementById("Register").style.display = "none";   
+     }
+     else {
+        document.getElementById("Logout").style.display = "none";
+     }
+ }
--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.set('views', 'Content');
 
 //session config
 app.use(sessions({
-    secret : secret,
-    saveUninitialized : true,
-    cookie : {maxAge : 1000 * 60 * 60 * 24},
-    resave : false
+    secret: secret,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false
 }));
 
 app.use(cookieParser());
@@ -37,28 +37,28 @@ function creatSession(userInfo, req, res) {
 
 // Routes
 app.get("/", function (req, res) {
-    res.render("index.html");
+    res.render("index.html", { user: req.session.user });
 });
 
 app.post("/", function (req, res) {
     var inputs = req.body.search;
     console.log(inputs);
-    res.render("index.html");
+    res.render("index.html", { user: req.session.user });
 });
 
 
 
 app.get("/login", function (req, res) {
-    if(req.session.user) {
+    if (req.session.user) {
         res.redirect("/");
     } else {
         res.render("login.html");
     }
 });
 
-app.post("/login", async function (req, res) {    
+app.post("/login", async function (req, res) {
     var userInfo = await db.getFromCollection("UserInfo", "users", { username: req.body.username });
-    
+
     if (userInfo != null) {
         if (await bcrypt.compare(req.body.password, userInfo.password)) {
             console.log("Login Successful");
@@ -72,7 +72,7 @@ app.post("/login", async function (req, res) {
 });
 
 app.get("/register", function (req, res) {
-    if(req.session.user) {
+    if (req.session.user) {
         res.redirect("/");
     } else {
         res.render("register.html");
@@ -82,16 +82,16 @@ app.get("/register", function (req, res) {
 app.post("/register", async function (req, res) {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
-    
+
     var UserExist = await db.isInCollection("UserInfo", "users", { username: req.body.username });
     var mailExist = await db.isInCollection("UserInfo", "users", { email: req.body.email });
-    
+
     if (UserExist) {
         res.render("register.html", { error: "Username have already been used" });
     } else if (mailExist) {
         res.render("register.html", { error: "Email have already been used" });
     } else {
-        db.addToDB("UserInfo", "users", { username: req.body.username, password: hash, email: req.body.email, poidsEnHabricot : req.body.poidsEnHabricot });
+        db.addToDB("UserInfo", "users", { username: req.body.username, password: hash, email: req.body.email, poidsEnHabricot: req.body.poidsEnHabricot });
         creatSession(await db.getFromCollection("UserInfo", "users", { username: req.body.username }), req, res);
     }
 });
